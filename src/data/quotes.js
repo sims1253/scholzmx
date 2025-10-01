a6d8c8f4623e3cd235a0f99ad9d6f91d88062625
Maximilian Scholz <dev.scholz@mailbox.org>
2025-10-01 22:19:45 +0200

    Fix foldable code blocks and more quotes

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -46,7 +46,7 @@ export const personalQuotes = [
     author: 'John Ringo',
   },
   {
-    text: 'Do not go gentle into that good night, old age should burn and rave at close of day; Rage, rage against the dying of the light.',
+    text: 'Do not go gentle into that good night,\nOld age should burn and rave at close of day;\nRage, rage against the dying of the light.',
     author: 'Dylan Thomas',
   },
   {
@@ -57,4 +57,77 @@ export const personalQuotes = [
     text: 'How do I smell, to be the king of dragonflies and bees?',
     author: 'Memories',
   },
+  {
+    text: 'No wake and no shelter\nWe are eroded\n I remember\nHaven has two faces\nOne is to create and one is to replace it',
+    author: 'Courtney LaPlante',
+  },
+  {
+    text: 'I am the space between\nShedding, free from skin and bone',
+    author: 'Buster Odeholm',
+  },
+  {
+    text: 'Placelessness\nUnbound by the weight of finity\nThrough the depths, suspended',
+    author: 'Buster Odeholm',
+  },
+  {
+    text: 'A seam has opened\nWide, for all to see\nAscend, reach divinity',
+    author: 'Buster Odeholm',
+  },
+  {
+    text: "I need to see their fear\nI need them to understand\nLet the world sing in despair\nWe don't belong here anymore\nIn time they will know",    author: 'Buster Odeholm',
+  },
+  {
+    text: "Fall on me amber moon",    author: 'Lars Marcus Johansson-Vik',
+  },
+  {
+    text: "I am enough\nI am whole\I am enough",    author: 'Lars Marcus Johansson-Vik',
+  },
+  {
+    text: "Well, somebody has to fight entropy.",    author: 'Richard Torkar',
+  },
+  {
+    text: "What benefit is eloquence\nIf I can't call and tell my friends\nThat I love them?\nCause I love them",
+    author: 'Thomas Wwaver',
+  },
+  {
+    text: "I hope you still remember every morning rain\nAnd if there ever was beauty in my eyes, in me",
+    author: 'Sadness',
+  },
+  {
+    text: "There's no time left to waste now that you're finally awake\nYou took a lifetime to deprogram so let's cut to the chase\nI'd say there's less than seven seconds 'til the bastards breach the door\nJust remember nothing's real, they can't hurt you anymore\nRun!",
+    author: 'Garret Russell',
+  },
+  {
+    text: "Intricate: I watched the world dance inside your head\nEphemeral: everything created must expire\nMisery: losing hope for a dying world\nOr did we lose hope in ourselves, my ineffable?",
+    author: 'Garret Russell',
+  },
+  {
+    text: "I wonder if we could fill\nThe gaps between the stars\nWith everything left unsaid\nIt's gonna tear us apart",
+    author: 'Garret Russell',
+  },
+  ,
+  {
+    text: "If you're breathing again, you'll hold your vows and say goodbye\nIf you're breathing again, spell out the world with open eyes\nAnd speak with the caution of colliding satellites\n",
+    author: 'Garret Russell',
+  },
+  {
+    text: "To the flowwers",
+    author: 'Sean Long, Aaran McKenzie',
+  },
+  {
+    text: "I might cry a bit afterwards, but— you really think a normal girl from Los Angeles can't slay a dragon?",
+    author: 'Allison Ruth (Tom Bloom)',
+  },
+  {
+    text: "What would finishing your project look like if it was an act of self-love instead of self-violation?",
+    author: 'Esmeralda',
+  },
+  {
+    text: "There's a point to all of this\nAnd it's always been you",
+    author: 'Michael McGough, Joel Quartuccio',
+  },
+  {
+    text: "People dance under the moon\nIn circles\nBone to rune\nAnd Skull to dust\nIce melts the thoughts of the gods\nRoot promises blossom",
+    author: 'Michael McGough, Joel Quartuccio',
+  }
 ];
